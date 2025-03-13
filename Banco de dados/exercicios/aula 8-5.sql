939389c1072a63c7e2b187065a3bb6bd114c5c1a
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-03-13 14:23:23 -0300

    Update aula 8-5.sql

--- Banco de dados/exercicios/aula 8-5.sql
+++ Banco de dados/exercicios/aula 8-5.sql
@@ -12,7 +12,37 @@ select crm, nome,especialidade
 from medicos
 where ativo = 'S';
 -- 3) Em seguida, criar um mecanismo para garantir que somente médicos ativos possam solicitar novos exames.
+create or replace  trigger fichas_medicos_ativos
+before insert or update on fichas
+for each row
+declare 
+    ex_medico_inativo exception;
+    crm_medico_qtd number;
+begin
+select count(*) INTO crm_medico_qtd
+    from medicos_ativos
+where crm = :NEW.crm;
+
+if(nvl(crm_medico_qtd, 0)=0) then
+    RAISE ex_medico_inativo
+end if;
+
+exception 
+    when ex_medico_inativo then
+    RAISE_APPLICATION_ERROR(-20225, 'Somente medicos ativos podem criar fichas e solicitar exames!');
+
+end;
 
 -- 4) Depois, experimentar:
 -- • Para um médico ativo, criar uma nova ficha e solicitar dois exames disponíveis quaisquer;
+insert into fichas (nro_ficha, cod_paciente, crm, data)
+values(99, 1, "38222", sysdate);
+
+insert into fichas_exames (nro_ficha,cod_exame)
+values(99,2)
+insert into fichas_exames (nro_ficha,cod_exame)
+values(99,3)
 -- • Marcar esse médico como aposentado e tentar repetir a ação anterior.
+update medicos
+set ativo = 'N'
+where crm = '38222';
